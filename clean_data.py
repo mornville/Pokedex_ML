8009df09e1adb3a0dec33b0a0e303b700e818191
Ashutosh Jha <aj97389@gmail.com>
2020-05-16 19:08:18 +0530

    Added pokemon dataset with 13,586 Train and 4,070 test images

--- clean_data.py
+++ clean_data.py
@@ -4,19 +4,19 @@ import os
 import cv2
 subdirs = []
 images=[]
-subdirs = os.listdir('dataset/train/')
+subdirs = os.listdir('dataset/test/')
 # print(subdirs)
 for item in subdirs:
     if(item!='.DS_Store'):
         print('\n------------------\n')
         print('\nChecking for: ' + str(item))
         print('\n------------------\n')
-        images = os.listdir('dataset/train/' + str(item) + '/')
+        images = os.listdir('dataset/test/' + str(item) + '/')
         print('Containes ' + str(len(images)) + ' images')
         for i in images:
             if(not(i.endswith('.png') or i.endswith('.gif') or i.endswith('.jpeg') or i.endswith('.jpg') or i.endswith('.png') or i.endswith('.gif') or i.endswith('.jpeg') or i.endswith('.JPG') or i.endswith('.png') or i.endswith('.gif') or i.endswith('.jpeg') or i.endswith('.JPEG') or i.endswith('.png') or i.endswith('.gif') or i.endswith('.jpeg') or i.endswith('.PNG'))):
                 print('Removing: ' + str(i))
                 '''The files removed by the next command cant be recovered by any 
                 means, check the path carefully'''
-                # os.remove('dataset/train/' + str(item) + '/' + str(i))
+                os.remove('dataset/test/' + str(item) + '/' + str(i))
         
